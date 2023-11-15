7b7be8223dbbc16315403b81e772e37cda2c6f57
DESKTOP-00T7TJQ\Emmanuel Malavassi <emalavassi260957@ufidelitas.ac.cr>
2023-11-15 15:35:47 -0600

    dding cotizador

--- send_email.php
+++ send_email.php
@@ -6,7 +6,7 @@ $status = '';
 $msg = '';
 
 if (array_key_exists('email', $_POST)) {
-    require './assets/vendor/autoload.php';
+    require 'vendor/autoload.php';
 
     // Input sanitization
     $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
@@ -23,74 +23,75 @@ if (array_key_exists('email', $_POST)) {
     $mail = new PHPMailer;
     $mail->isSMTP();
     $mail->Host = 'smtp.hostinger.com';
-    $mail->Port = 465;
-    $mail->SMTPDebug = 1;
+    $mail->Port = 587;
+    $mail->SMTPDebug = 0;
     $mail->SMTPAuth = true;
     $mail->Username = 'arquitectstudio@arquitectstudio.online';
     $mail->Password = 'Qov53685$';
-    $mail->setFrom('arquitectstudio@arquitectstudio.online', $name . ' ' . $apellido);  //aqui llegan los correos
+    $mail->setFrom('arquitectstudio@arquitectstudio.online', $name);  //aqui llegan los correos
     $mail->addAddress('arquitectwebservices@arquitectstudio.com');
 
     if ($mail->addReplyTo($email, $name)) {
-        $mail->Subject = 'PHPMailer contact form';
+        $mail->Subject = $subject;
         $mail->isHTML(true);
-        $mail->Body =  <<<EOT
-    <table style="border: 1px solid black; border-collapse: collapse; width: 100%;">
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Nombre</th>
-            <td style="border: 1px solid black; padding: 8px;">$name</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Apellido</th>
-            <td style="border: 1px solid black; padding: 8px;">$apellido</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Email</th>
-            <td style="border: 1px solid black; padding: 8px;">$email</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Phone Number</th>
-            <td style="border: 1px solid black; padding: 8px;">$phone</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Propiedad</th>
-            <td style="border: 1px solid black; padding: 8px;">$propertyType</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Construccion</th>
-            <td style="border: 1px solid black; padding: 8px;">$constructionType</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Cantidad de Metros Cuadros</th>
-            <td style="border: 1px solid black; padding: 8px;">$squareMeters</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Acabados</th>
-            <td style="border: 1px solid black; padding: 8px;">$acabados</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Rango de Presupuesto</th>
-            <td style="border: 1px solid black; padding: 8px;">$presupuesto</td>
-        </tr>
-        <tr>
-            <th style="border: 1px solid black; padding: 8px; text-align: left;">Descripción del Proyecto</th>
-            <td style="border: 1px solid black; padding: 8px;">$message_area</td>
-        </tr>
-    </table>
-EOT;
-       
-if (!$mail->send()) {
-    $msg = 'Mailer Error: ' . $mail->ErrorInfo;
-    $status = 'error';
-} else {
-    $msg = 'Thank you for contacting us. Your message has been successfully sent!';
-    $status = 'success';
-    header('Location: index.html');
-    exit();
-}
-} else {
-$msg = 'Please provide a valid reply-to email.';
-$status = 'error';
-}
+        $mail->Body = <<<EOT
+        <table style="border: 1px solid black; border-collapse: collapse; width: 100%;">
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Nombre</th>
+                <td style="border: 1px solid black; padding: 8px;">$name</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Apellido</th>
+                <td style="border: 1px solid black; padding: 8px;">$apellido</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Email</th>
+                <td style="border: 1px solid black; padding: 8px;">$email</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Phone Number</th>
+                <td style="border: 1px solid black; padding: 8px;">$phone</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Propiedad</th>
+                <td style="border: 1px solid black; padding: 8px;">$propertyType</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Construccion</th>
+                <td style="border: 1px solid black; padding: 8px;">$constructionType</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Cantidad de Metros Cuadros</th>
+                <td style="border: 1px solid black; padding: 8px;">$squareMeters</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Tipo de Acabados</th>
+                <td style="border: 1px solid black; padding: 8px;">$acabados</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Rango de Presupuesto</th>
+                <td style="border: 1px solid black; padding: 8px;">$presupuesto</td>
+            </tr>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px; text-align: left;">Descripción del Proyecto</th>
+                <td style="border: 1px solid black; padding: 8px;">$message_area</td>
+            </tr>
+        </table>
+    EOT;
+
+if ($mail->send()) {
+            // Redirect immediately on success
+            echo '<meta http-equiv="refresh" content="1;url=index.html">';
+            exit();
+        } else {
+            // Set error status and message
+            $status = 'error';
+            $msg = 'There was a problem sending the email. Please try again later.';
+        }
+    } else {
+        // Set error status and message for an invalid reply-to email
+        $status = 'error';
+        $msg = 'Invalid reply-to email.';
+    }
 }
 ?>
